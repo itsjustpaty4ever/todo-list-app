a3d1a061c146a0fbe91873009c01570e98895345
paty3 <itsjustpaty4ever@gmail.com>
2025-10-30 17:51:54 +0000

    impleented add task

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("Script started");
+
+function addTask(event){
+    //get user input from text box
+    let textBox = document.getElementById("task-input");
+    let task = textBox.value;
+    
+    //create list item
+    //get todo list container div
+    let todoList = document.getElementById("todo-list")
+
+    //create list item div
+    let listDiv = document.createElement("div");
+    listDiv.id = "task" + todoList.childElementCount;
+    listDiv.classList.add("list-item");
+
+    // create checkbox
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "checkbox" + todoList.childElementCount;
+
+    //create label
+    let label = document.createElement("label");
+    label.id = "label1" + todoList.childElementCount;
+    label.innerText = task;
+
+    //append checkbox to list item div
+    listDiv.appendChild(checkbox);
+
+    //append label to div
+    listDiv.appendChild(label);
+
+    //append list item item div to list
+    todoList.appendChild(listDiv);
+}
